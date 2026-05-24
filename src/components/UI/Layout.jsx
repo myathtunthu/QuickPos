a482e02882ca8cd11dc6db34d145666f2778ece1
myathtunthu <94426091+myathtunthu@users.noreply.github.com>
2026-05-24 14:24:13 +0630

    Update Layout.jsx

--- src/components/UI/Layout.jsx
+++ src/components/UI/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar';
+import Sidebar from './Sidebar';
 import BottomNav from '../BottomNav';
 import { Menu, X } from 'lucide-react';
 
